78fbe09bfc52adb33efa28b263de8d69d59b2b66
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-02-11 22:25:52 +0530

    update

--- dictionary.py
+++ dictionary.py
@@ -20,11 +20,18 @@ x = d2.keys()       #Fetching keys
 y = d2.values()     #Fetching values
 print(x,y,sep="\n")
 
-
-
 #Removing
 del d1[1]
 d2.pop("a")     #Removing with key
 d2.popitem()    #Remove the last item
 print(d2)
 #d1.clear()     #Wipe out he entire dictionary
+
+#Detecting a value or key if it exist in the  dict by iterating 
+di = dict(a = "Gaming",b = "Finance",c = "Politics",d = "Geography")
+
+
+if "Politics" in di.values() and "a" in di.keys():
+    print(True)
+else:
+    print(False)
