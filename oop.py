092dd17b3b09a221f7cbd0ea249a68f3bb86bfec
Himjyoti Talukdar <himzyotitalukdar@gmail.com>
2025-02-17 21:29:03 +0530

    update

--- oop.py
+++ oop.py
@@ -1,10 +1,10 @@
 #Class and object
 class car: 
-    brand = "Mercedes"          # Class attribute
+    brand = "Mercedes"          # Class attribute / variable
 
     def __init__(self,model,year):
-        self.model = model      # Instance attribute
-        self.year = year        # Instance attribute
+        self.model = model      # Instance attribute / variable
+        self.year = year        # Instance attribute / variable
 
 vehicle = car("GlS 450",2023)
 print(vehicle.model,vehicle.year, sep=" & ")
